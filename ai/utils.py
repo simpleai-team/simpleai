f8535654f04f37b482e0dca9918c65f0b53af0f0
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-08-03 21:03:49 -0300

    Created add once list, to use on graph search

--- ai/utils.py
+++ ai/utils.py
@@ -6,3 +6,22 @@ class FifoList(list):
 
     def pop(self, index=0):
         return super(FifoList, self).pop(index)
+
+
+class AddOnceList(list):
+    '''List which doesn't allow adding two times the same element,
+    even when the element isn't present on the list any more.'''
+
+    def __init__(self, *args, **kargs):
+        super(AddOnceList, self).__init__(*args, **kargs)
+        self.memory = set()
+
+    def append(self, element):
+        if hash(element) not in self.memory:
+            super(AddOnceList, self).append(element)
+            self.memory.add(hash(element))
+
+    def extend(self, iterable):
+        new_elements = [x for x in iterable
+                        if hash(x) not in self.memory]
+        super(AddOnceList, self).extend(new_elements)
