22ba54a6bf5bbbcbf48bb991cab068ab4324346c
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-09-20 14:50:06 -0300

    Basic csp implementation, without constraint propagations

---
+++ simpleai/csp.py
@@ -0,0 +1,103 @@
+# coding=utf-8
+from copy import deepcopy
+from itertools import product
+from simpleai.models import CspAssignment
+
+"hiperarc_consistency = tomo cada var, y me fijo que para todas las restricciones todos sus valores tengan posibles valores en el resto de las variables restringidas"
+
+MOST_CONSTRAINED_VARIABLE = 'mcv'
+HIGHEST_DEGREE_VARIABLE = 'degree'
+LEAST_CONSTRAINING_VALUE = 'lvc'
+
+
+def backtrack(csp_problem, variable_heuristic='', value_heuristic=''):
+    assignment = {}
+    domains = deepcopy(csp_problem.domains)
+
+    if variable_heuristic == MOST_CONSTRAINED_VARIABLE:
+        variable_chooser = _most_constrained_variable_chooser
+    elif variable_heuristic == HIGHEST_DEGREE_VARIABLE:
+        variable_chooser = _highest_degree_variable_chooser
+    else:
+        variable_chooser = _basic_variable_chooser
+
+    if value_heuristic == LEAST_CONSTRAINING_VALUE:
+        values_sorter = _least_constraining_values_sorter
+    else:
+        values_sorter = _basic_values_sorter
+
+    return _backtracking(csp_problem,
+                         assignment,
+                         domains,
+                         variable_chooser,
+                         values_sorter)
+
+
+def _basic_variable_chooser(problem, variables, domains):
+    return variables[0]
+
+
+def _most_constrained_variable_chooser(problem, variables, domains):
+    # the variable with fewer values available
+    return sorted(variables, key=lambda v: len(domains[v]))[0]
+
+
+def _highest_degree_variable_chooser(problem, variables, domains):
+    # the variable involved in more constraints
+    return sorted(variables, key=lambda v: problem.var_degrees[v], reverse=True)[0]
+
+
+def _count_conflicts(problem, assignment, variable, value):
+    conflicts = 0
+    new_assignment = deepcopy(assignment)
+    new_assignment[variable] = value
+
+    for neighbors, constraint in problem.var_contraints[variable]:
+        # if all the neighbors on the constraint have values, check if conflict
+        if all(n in new_assignment for n in neighbors):
+            variables, values = zip(*[(n, new_assignment[n])
+                                      for n in neighbors])
+            if not constraint(variables, values):
+                conflicts += 1
+
+    return conflicts
+
+
+def _basic_values_sorter(problem, variable, domains):
+    return domains[variable][:]
+
+
+def _least_constraining_values_sorter(problem, variable, domains):
+    # the value that generates less conflicts
+    values = sorted(domains[variable][:],
+                    key=lambda v: _count_conflicts(variable, v))
+    return values
+
+
+def _backtracking(problem, assignment, domains, variable_chooser,
+                  values_sorter):
+    if len(assignment) == len(problem.variables):
+        return assignment
+
+    pending = [v for v in problem.variables
+               if v not in assignment]
+    variable = variable_chooser(problem, pending, domains)
+
+    values = values_sorter(problem, variable, domains)
+
+    for value in values:
+        if not _count_conflicts(problem, assignment, variable, value): # TODO on aima also checks if using fc
+            new_assignment = deepcopy(assignment)
+            new_domains = deepcopy(domains)
+
+            new_assignment[variable] = value
+
+            # TODO propagation and inferences
+
+            return _backtracking(problem,
+                                 new_assignment,
+                                 new_domains,
+                                 variable_chooser,
+                                 values_sorter)
+
+    return None
--- simpleai/models.py
+++ simpleai/models.py
@@ -144,3 +144,20 @@ class SearchNodeStarOrdered(SearchNode):
     def __lt__(self, other):
         return self.problem.heuristic(self.state) + self.cost < \
                self.problem.heuristic(other.state) + other.cost
+
+
+class CspProblem(object):
+    def __init__(self, variables, domains, constraints):
+        self.variables = variables
+        self.domains = domains
+        self.constraints = constraints
+
+        # variable-based constraints dict
+        self.var_contraints = dict([(v, [constraint
+                                         for constraint in constraints
+                                         if v in constraint[0]])
+                                    for v in variables])
+
+        # calculate degree of each variable
+        self.var_degrees = dict([(v, len(self.var_contraints[v]))
+                                 for v in variables])
