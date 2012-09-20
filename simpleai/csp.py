c7be6fea0d55efc40fb5499e5ab7b9fc6f32eec5
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-09-20 16:56:51 -0300

    Initial implementation of csp local search (min_conflicts algorithm)

--- simpleai/csp.py
+++ simpleai/csp.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import random
 from copy import deepcopy
 
 
@@ -45,18 +46,21 @@ def _highest_degree_variable_chooser(problem, variables, domains):
 
 
 def _count_conflicts(problem, assignment, variable=None, value=None):
+    return len(_find_conflicts(problem, assignment, variable, value))
+
+def _find_conflicts(problem, assignment, variable=None, value=None):
     if variable and value:
         assignment = deepcopy(assignment)
         assignment[variable] = value
 
-    conflicts = 0
+    conflicts = []
     for neighbors, constraint in problem.constraints:
         # if all the neighbors on the constraint have values, check if conflict
         if all(n in assignment for n in neighbors):
             variables, values = zip(*[(n, assignment[n])
                                       for n in neighbors])
             if not constraint(variables, values):
-                conflicts += 1
+                conflicts.append((neighbors, constraint))
 
     return conflicts
 
@@ -105,3 +109,41 @@ def _backtracking(problem, assignment, domains, variable_chooser,
                                  values_sorter)
 
     return None
+
+
+def _min_conflicts_value(problem, assignement, variable):
+    """Return the value that will give var the least number of conflicts.
+    If there is a tie, choose at random."""
+    return sorted(problem.domains[variable],
+                  key=lambda v: _count_conflicts(problem, assignement,
+                                                 variable, v))[0]
+
+
+def min_conflicts(problem, initial_assignment=None, iterations_limit=0):
+    assignement = {}
+    if initial_assignment:
+        assignement.update(initial_assignment)
+    else:
+        for variable in problem.variables:
+            value = _min_conflicts_value(problem, assignement, variable)
+            assignement[variable] = value
+
+    iteration = 0
+    run = True
+    while run:
+        conflicts = _find_conflicts(problem, assignement)
+
+        conflict_variables = set(sum([conflict[0] for conflict in conflicts],
+                                     []))
+        variable = random.choice(conflict_variables)
+        value = _min_conflicts_value(problem, assignement, variable)
+        assignement[variable] = value
+
+        iteration += 1
+
+        if iterations_limit and iteration >= iterations_limit:
+            run = False
+        elif not _count_conflicts(problem, assignement):
+            run = False
+
+    return assignement
