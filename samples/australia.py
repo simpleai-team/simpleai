58551169b61a3dc2e91cfbb30a6e05b535d28e5f
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-09-27 20:51:43 -0300

    Added csp sample (map coloring of australia

---
+++ samples/australia.py
@@ -0,0 +1,31 @@
+from simpleai.models import CspProblem
+from simpleai.csp import backtrack, min_conflicts, MOST_CONSTRAINED_VARIABLE, HIGHEST_DEGREE_VARIABLE, LEAST_CONSTRAINING_VALUE
+
+variables = ('WA', 'NT', 'SA', 'Q', 'NSW', 'V', 'T')
+
+domains = dict((v, ['red', 'green', 'blue']) for v in variables)
+
+def const_different(variables, values):
+    return values[0] == values[1]  # expect the value of the neighbors to be different
+
+constraints = [
+    (('WA', 'NT'), const_different),
+    (('WA', 'SA'), const_different),
+    (('SA', 'NT'), const_different),
+    (('SA', 'Q'), const_different),
+    (('NT', 'Q'), const_different),
+    (('SA', 'NSW'), const_different),
+    (('Q', 'NSW'), const_different),
+    (('SA', 'V'), const_different),
+    (('NSW', 'V'), const_different),
+]
+
+my_problem = CspProblem(variables, domains, constraints)
+
+print backtrack(my_problem)
+print backtrack(my_problem, variable_heuristic=MOST_CONSTRAINED_VARIABLE)
+print backtrack(my_problem, variable_heuristic=HIGHEST_DEGREE_VARIABLE)
+print backtrack(my_problem, value_heuristic=LEAST_CONSTRAINING_VALUE)
+print backtrack(my_problem, variable_heuristic=MOST_CONSTRAINED_VARIABLE, value_heuristic=LEAST_CONSTRAINING_VALUE)
+print backtrack(my_problem, variable_heuristic=HIGHEST_DEGREE_VARIABLE, value_heuristic=LEAST_CONSTRAINING_VALUE)
+print min_conflicts(my_problem)
