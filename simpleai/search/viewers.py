fa65cdcb8fb657937bf8ccf1213d1d5864741007
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-01-21 23:02:38 -0300

    Simple proof of concept of WebViewer based on ConsoleViewer (same output, web-based)

--- simpleai/search/viewers.py
+++ simpleai/search/viewers.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+from threading import Thread
+
 
 class DummyViewer(object):
     def start(self):
@@ -38,4 +40,54 @@ class ConsoleViewer(DummyViewer):
     def expanded(self, node, successors):
         print 'Expand:', node
         print len(successors), 'successors:', successors
+        self.pause()
+
+
+class WebViewer(DummyViewer):
+    def __init__(self):
+        self.paused = True
+        self.events = []
+
+    def start(self):
+        from bottle import route, run, redirect
+
+        @route('/')
+        def status():
+            return '<a href="/next">Next</a> <br />Status:<br />' + '<br />'.join(self.events)
+
+        @route('/next')
+        def next():
+            self.paused = False
+            redirect('/')
+
+        t = Thread(target=run)
+        t.daemon = True
+        t.start()
+
+        self.pause()
+
+    def pause(self):
+        self.paused = True
+        while self.paused:
+            pass
+
+    def event(self, *args):
+        self.events.append(' '.join(map(str, args)))
+
+    def new_iteration(self, fringe):
+        self.event(' **** New iteration ****')
+        self.event(len(fringe), 'elements in fringe:', fringe)
+        self.pause()
+
+    def chosen_node(self, node, is_goal):
+        self.event('Chosen node:', node)
+        if is_goal:
+            self.event('Is goal!')
+        else:
+            self.event('Not goal')
+        self.pause()
+
+    def expanded(self, node, successors):
+        self.event('Expand:', node)
+        self.event(len(successors), 'successors:', successors)
         self.pause()
