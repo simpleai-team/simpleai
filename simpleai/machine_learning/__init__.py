4aeaeaf5aef676debb01705bf4386a65230a065a
Francisco Capdevila <fjcapdevila@gmail.com>
2013-07-05 15:58:22 -0300

    Make PEP8 compliant

--- simpleai/machine_learning/__init__.py
+++ simpleai/machine_learning/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from simpleai.machine_learning.models import ClassificationProblem, \
-                                             VectorDataClassificationProblem, \
-                                             Attribute, VectorIndexAttribute, \
-                                             is_attribute, \
-                                             Classifier
-from simpleai.machine_learning.classifiers import DecisionTreeLearner, \
-                                                  DecisionTreeLearner_Queued, \
-                                               DecisionTreeLearner_LargeData, \
-                                                  NaiveBayes, \
-                                                  KNearestNeighbors
+from simpleai.machine_learning.models import (ClassificationProblem,
+                                              VectorDataClassificationProblem,
+                                              Attribute, VectorIndexAttribute,
+                                              is_attribute,
+                                              Classifier)
+from simpleai.machine_learning.classifiers import (DecisionTreeLearner,
+                                                   DecisionTreeLearner_Queued,
+                                                   DecisionTreeLearner_LargeData,
+                                                   NaiveBayes,
+                                                   KNearestNeighbors)
 from simpleai.machine_learning.evaluation import precision, kfold
