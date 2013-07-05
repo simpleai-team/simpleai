c4ad0a6ed1e1bfb90e8ab6b6472c6ce4ce3b47e7
Francisco Capdevila <fjcapdevila@gmail.com>
2013-07-05 16:20:10 -0300

    Make PEP8 compliant

--- tests/search/test_csp.py
+++ tests/search/test_csp.py
@@ -19,21 +19,25 @@ class TestCsp(unittest.TestCase):
             'C': [1, 2],
         }
 
-        # a constraint that expects different variables to have different values
+        # a constraint that expects different variables to have
+        # different values
         def const_different(variables, values):
-            return len(values) == len(set(values))  # remove repeated values and count
+            # remove repeated values and count
+            return len(values) == len(set(values))
 
         # a constraint that expects one variable to be bigger than other
         def const_one_bigger_other(variables, values):
             return values[0] > values[1]
 
-        # a constraint thet expects two variables to be one odd and the other even,
-        # no matter which one is which type
+        # a constraint thet expects two variables to be one odd and
+        # the other even, no matter which one is which type
         def const_one_odd_one_even(variables, values):
             if values[0] % 2 == 0:
-                return values[1] % 2 == 1  # first even, expect second to be odd
+                # first even, expect second to be odd
+                return values[1] % 2 == 1
             else:
-                return values[1] % 2 == 0  # first odd, expect second to be even
+                # first odd, expect second to be even
+                return values[1] % 2 == 0
 
         # a constraint that requires one variable to be different than 1
         def const_not_1(variables, values):
@@ -46,14 +50,19 @@ class TestCsp(unittest.TestCase):
             (('A',), const_not_1)
         ]
 
-        self.problem = CspProblem(self.variables, self.domains, self.constraints)
+        self.problem = CspProblem(self.variables, self.domains,
+                                  self.constraints)
 
     def test_most_constrained_variable_chooser(self):
-        variable = _most_constrained_variable_chooser(self.problem, self.variables, self.domains)
+        variable = _most_constrained_variable_chooser(self.problem,
+                                                      self.variables,
+                                                      self.domains)
         self.assertEquals(variable, 'C')
 
     def test_highest_degree_variable_chooser(self):
-        variable = _highest_degree_variable_chooser(self.problem, self.variables, self.domains)
+        variable = _highest_degree_variable_chooser(self.problem,
+                                                    self.variables,
+                                                    self.domains)
         self.assertEquals(variable, 'A')
 
     def test_find_conflicts(self):
@@ -78,7 +87,8 @@ class TestCsp(unittest.TestCase):
 
     def test_least_constraining_values_sorter(self):
         assignment = {'A': 1, 'B': 1}
-        values = _least_constraining_values_sorter(self.problem, assignment, 'C', self.domains)
+        values = _least_constraining_values_sorter(self.problem, assignment,
+                                                   'C', self.domains)
         self.assertEquals(values, [2, 1])
 
     def test_min_conflicts_value(self):
@@ -88,8 +98,8 @@ class TestCsp(unittest.TestCase):
 
     def test_backtrack(self):
         result = backtrack(self.problem)
-        self.assertEqual(result, {'A':2, 'B': 3, 'C': 1})
+        self.assertEqual(result, {'A': 2, 'B': 3, 'C': 1})
 
     def test_min_conflicts(self):
         result = min_conflicts(self.problem)
-        self.assertEqual(result, {'A':2, 'B': 3, 'C': 1})
+        self.assertEqual(result, {'A': 2, 'B': 3, 'C': 1})
