b2d24326aebd2c351662e55044786eafde7a7581
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-11-23 19:30:20 -0300

    Removed another unused function

--- simpleai/search/arc.py
+++ simpleai/search/arc.py
@@ -3,45 +3,9 @@ from operator import itemgetter
 
 
 # The first 3 functions are exported for testing purposes.
-__all__ = ['neighbors', 'all_arcs', 'revise', 'arc_consistency_3']
+__all__ = ['all_arcs', 'revise', 'arc_consistency_3']
 
 fst = itemgetter(0)
-
-
-def neighbors(xi, constraints, exclude):
-    '''
-    Returns all variables that are related, through a constraint, to @xi
-
-    @constraints is a list of tuples, each tuple having
-    a tuple of variables and a callable which serves as
-    the constraint between the variables. e.g:
-
-    [(('X', 'Y'), lambda vars, values: values[0] == values[1]), .. ]
-
-    If the constraint was as above, 'Y' would be a neighbor of 'X'.
-    '''
-    def _neighbor(variables):
-        idx_i = variables.index(xi)
-        idx_j = 1 - idx_i
-        neighbor = variables[idx_j]
-        return neighbor
-
-    seen = set()
-    for t in constraints:
-        variables, func = t
-
-        if len(variables) != 2:  # ignore constraints that are not binary
-            continue
-
-        if not xi in variables:
-            continue
-        xk = _neighbor(variables)
-        if xk == exclude:
-            continue
-        if xk in seen:
-            continue
-        seen.add(xk)
-        yield ((xk, xi), func)
 
 
 def revise(domains, variables, constraint):
