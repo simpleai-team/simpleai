cfec84681bb91c0203cd40c7f49439808ae407c9
Jose Cabrera <jose.cabrera@bigbox.com.ar>
2021-11-21 18:17:34 -0300

    fix: before resize the tree graph

---
+++ simpleai/search/web_viewer_resources/js/renderGraph.js
@@ -0,0 +1,330 @@
+var treeData = [
+    {
+        "children": [
+            {
+                "children": [
+                    {
+                        "children": [
+                            {
+                                "children": [
+                                    {
+                                        "children": [
+                                            {
+                                                "children": [],
+                                                "name": "((3, 7, 1), (8, 0, 2), (4, 6, 5))",
+                                                "new_child": true,
+                                                "tooltip": "\nCost: 5\nHeuristic: 12"
+                                            },
+                                            {
+                                                "children": [],
+                                                "name": "((0, 3, 1), (8, 7, 2), (4, 6, 5))",
+                                                "new_child": true,
+                                                "tooltip": "\nCost: 5\nHeuristic: 12"
+                                            },
+                                            {
+                                                "children": [],
+                                                "name": "((3, 1, 0), (8, 7, 2), (4, 6, 5))",
+                                                "new_child": true,
+                                                "tooltip": "\nCost: 5\nHeuristic: 10"
+                                            }
+                                        ],
+                                        "expanded": true,
+                                        "in_fringe": true,
+                                        "name": "((3, 0, 1), (8, 7, 2), (4, 6, 5))",
+                                        "tooltip": "\nCost: 4\nHeuristic: 11"
+                                    },
+                                    {
+                                        "children": [],
+                                        "in_fringe": true,
+                                        "name": "((3, 7, 1), (0, 8, 2), (4, 6, 5))",
+                                        "tooltip": "\nCost: 4\nHeuristic: 11"
+                                    },
+                                    {
+                                        "children": [],
+                                        "in_fringe": true,
+                                        "name": "((3, 7, 1), (8, 6, 2), (4, 0, 5))",
+                                        "tooltip": "\nCost: 4\nHeuristic: 13"
+                                    }
+                                ],
+                                "name": "((3, 7, 1), (8, 0, 2), (4, 6, 5))",
+                                "tooltip": "\nCost: 3\nHeuristic: 12"
+                            },
+                            {
+                                "children": [],
+                                "in_fringe": true,
+                                "name": "((3, 7, 1), (8, 2, 5), (4, 6, 0))",
+                                "tooltip": "\nCost: 3\nHeuristic: 12"
+                            }
+                        ],
+                        "name": "((3, 7, 1), (8, 2, 0), (4, 6, 5))",
+                        "tooltip": "\nCost: 2\nHeuristic: 13"
+                    }
+                ],
+                "name": "((3, 7, 0), (8, 2, 1), (4, 6, 5))",
+                "tooltip": "\nCost: 1\nHeuristic: 14"
+            },
+            {
+                "children": [
+                    {
+                        "children": [
+                            {
+                                "children": [
+                                    {
+                                        "children": [
+                                            {
+                                                "children": [
+                                                    {
+                                                        "children": [],
+                                                        "in_fringe": true,
+                                                        "name": "((3, 2, 7), (4, 1, 0), (6, 8, 5))",
+                                                        "tooltip": "\nCost: 6\nHeuristic: 9"
+                                                    },
+                                                    {
+                                                        "children": [
+                                                            {
+                                                                "children": [],
+                                                                "in_fringe": true,
+                                                                "name": "((0, 2, 7), (3, 4, 1), (6, 8, 5))",
+                                                                "tooltip": "\nCost: 7\nHeuristic: 8"
+                                                            },
+                                                            {
+                                                                "children": [],
+                                                                "in_fringe": true,
+                                                                "name": "((3, 2, 7), (6, 4, 1), (0, 8, 5))",
+                                                                "tooltip": "\nCost: 7\nHeuristic: 10"
+                                                            }
+                                                        ],
+                                                        "name": "((3, 2, 7), (0, 4, 1), (6, 8, 5))",
+                                                        "tooltip": "\nCost: 6\nHeuristic: 9"
+                                                    },
+                                                    {
+                                                        "children": [],
+                                                        "in_fringe": true,
+                                                        "name": "((3, 0, 7), (4, 2, 1), (6, 8, 5))",
+                                                        "tooltip": "\nCost: 6\nHeuristic: 11"
+                                                    }
+                                                ],
+                                                "name": "((3, 2, 7), (4, 0, 1), (6, 8, 5))",
+                                                "tooltip": "\nCost: 5\nHeuristic: 10"
+                                            },
+                                            {
+                                                "children": [],
+                                                "in_fringe": true,
+                                                "name": "((3, 2, 7), (4, 8, 1), (6, 5, 0))",
+                                                "tooltip": "\nCost: 5\nHeuristic: 12"
+                                            }
+                                        ],
+                                        "name": "((3, 2, 7), (4, 8, 1), (6, 0, 5))",
+                                        "tooltip": "\nCost: 4\nHeuristic: 11"
+                                    }
+                                ],
+                                "name": "((3, 2, 7), (4, 8, 1), (0, 6, 5))",
+                                "tooltip": "\nCost: 3\nHeuristic: 12"
+                            },
+                            {
+                                "children": [
+                                    {
+                                        "children": [],
+                                        "in_fringe": true,
+                                        "name": "((2, 0, 7), (3, 8, 1), (4, 6, 5))",
+                                        "tooltip": "\nCost: 4\nHeuristic: 13"
+                                    }
+                                ],
+                                "name": "((0, 2, 7), (3, 8, 1), (4, 6, 5))",
+                                "tooltip": "\nCost: 3\nHeuristic: 12"
+                            }
+                        ],
+                        "name": "((3, 2, 7), (0, 8, 1), (4, 6, 5))",
+                        "tooltip": "\nCost: 2\nHeuristic: 13"
+                    },
+                    {
+                        "children": [
+                            {
+                                "children": [
+                                    {
+                                        "children": [
+                                            {
+                                                "children": [],
+                                                "in_fringe": true,
+                                                "name": "((3, 1, 2), (8, 0, 7), (4, 6, 5))",
+                                                "tooltip": "\nCost: 5\nHeuristic: 10"
+                                            },
+                                            {
+                                                "children": [],
+                                                "in_fringe": true,
+                                                "name": "((0, 3, 2), (8, 1, 7), (4, 6, 5))",
+                                                "tooltip": "\nCost: 5\nHeuristic: 12"
+                                            }
+                                        ],
+                                        "name": "((3, 0, 2), (8, 1, 7), (4, 6, 5))",
+                                        "tooltip": "\nCost: 4\nHeuristic: 11"
+                                    }
+                                ],
+                                "name": "((3, 2, 0), (8, 1, 7), (4, 6, 5))",
+                                "tooltip": "\nCost: 3\nHeuristic: 12"
+                            },
+                            {
+                                "children": [
+                                    {
+                                        "children": [],
+                                        "in_fringe": true,
+                                        "name": "((3, 2, 7), (8, 1, 5), (4, 0, 6))",
+                                        "tooltip": "\nCost: 4\nHeuristic: 13"
+                                    }
+                                ],
+                                "name": "((3, 2, 7), (8, 1, 5), (4, 6, 0))",
+                                "tooltip": "\nCost: 3\nHeuristic: 12"
+                            }
+                        ],
+                        "name": "((3, 2, 7), (8, 1, 0), (4, 6, 5))",
+                        "tooltip": "\nCost: 2\nHeuristic: 13"
+                    },
+                    {
+                        "children": [],
+                        "in_fringe": true,
+                        "name": "((3, 2, 7), (8, 6, 1), (4, 0, 5))",
+                        "tooltip": "\nCost: 2\nHeuristic: 15"
+                    }
+                ],
+                "name": "((3, 2, 7), (8, 0, 1), (4, 6, 5))",
+                "tooltip": "\nCost: 1\nHeuristic: 14"
+            },
+            {
+                "children": [],
+                "in_fringe": true,
+                "name": "((0, 3, 7), (8, 2, 1), (4, 6, 5))",
+                "tooltip": "\nCost: 1\nHeuristic: 16"
+            }
+        ],
+        "name": "((3, 0, 7), (8, 2, 1), (4, 6, 5))",
+        "tooltip": "\nCost: 0\nHeuristic: 15"
+    }
+
+];
+
+
+// ************** Generate the tree diagram	 *****************
+
+var margin = { top: 20, right: 120, bottom: 20, left: 120 },
+    width = 960 - margin.right - margin.left,
+    height = 500 - margin.top - margin.bottom;
+
+var i = 0,
+    duration = 750,
+    root;
+
+var tree = d3.layout.tree().size([width, height]);
+
+var diagonal = d3.svg.diagonal().projection(function (d) { return [d.x, d.y]; });
+
+var svg = d3.select("#graph-image").append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function update(source) {
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root).reverse(),
+        links = tree.links(nodes);
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function (d) { d.y = d.depth * 180; });
+
+    // Update the nodes…
+    var node = svg.selectAll("g.node")
+        .data(nodes, function (d) { return d.id || (d.id = ++i); });
+
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) { return "translate(" + source.x0 + "," + source.y0 + ")"; })
+        .on("click", click);
+
+    nodeEnter.append("circle")
+        .attr("r", 1e-6)
+        .style("fill", function (d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeEnter.append("text")
+        .attr("x", function (d) { return d.children || d._children ? -13 : 13; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function (d) { return d.children || d._children ? "end" : "start"; })
+        .text(function (d) { return d.name; })
+        .style("fill-opacity", 1e-6);
+
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition()
+        .duration(duration)
+        .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+    nodeUpdate.select("circle")
+        .attr("r", 10)
+        .style("fill", function (d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeUpdate.select("text")
+        .style("fill-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    var nodeExit = node.exit().transition()
+        .duration(duration)
+        .attr("transform", function (d) { return "translate(" + source.x + "," + source.y + ")"; })
+        .remove();
+
+    nodeExit.select("circle")
+        .attr("r", 1e-6);
+
+    nodeExit.select("text")
+        .style("fill-opacity", 1e-6);
+
+    // Update the links…
+    var link = svg.selectAll("path.link")
+        .data(links, function (d) { return d.target.id; });
+
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("path", "g")
+        .attr("class", "link")
+        .attr("d", function (d) {
+            var o = { x: source.x0, y: source.y0 };
+            return diagonal({ source: o, target: o });
+        });
+
+    // Transition links to their new position.
+    link.transition()
+        .duration(duration)
+        .attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+        .duration(duration)
+        .attr("d", function (d) {
+            var o = { x: source.x, y: source.y };
+            return diagonal({ source: o, target: o });
+        })
+        .remove();
+
+    // Stash the old positions for transition.
+    nodes.forEach(function (d) {
+        d.x0 = d.x;
+        d.y0 = d.y;
+    });
+}
+
+// Toggle children on click.
+function click(d) {
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+    } else {
+        d.children = d._children;
+        d._children = null;
+    }
+    update(d);
+}
+
+function render() {
+    root = treeData[0];
+    root.x0 = height / 2;
+    root.y0 = 0;
+    update(root);
+    d3.select(self.frameElement).style("height", "500px");
+}
