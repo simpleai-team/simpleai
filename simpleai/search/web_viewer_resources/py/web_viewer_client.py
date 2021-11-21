b15d97106ecdf7f1bcfb562017338699ea411d52
Juan Pedro Fisanotti <fisadev@gmail.com>
2021-11-21 16:02:02 -0300

    Log, last event and stats working on brython

--- simpleai/search/web_viewer_resources/py/web_viewer_client.py
+++ simpleai/search/web_viewer_resources/py/web_viewer_client.py
@@ -4,6 +4,8 @@ from browser import aio, window
 
 
 jq = window.jQuery
+EventSource = window.EventSource
+jsjson = window.JSON
 
 
 class Tab(Enum):
@@ -39,7 +41,16 @@ class WebViewerClient:
         jq(".control-algorithm-btn").on("click", self.on_control_algorithm_click)
         jq(".show-tab-btn").on("click", self.on_show_tab_click)
 
+        self.last_event = None
+        self.event_log = []
+        self.stats = {}
+
+        self.stats_display = jq("#stats-display")
+        self.last_event_display = jq("#last-event-display")
+        self.log_display = jq("#log-display")
+
         self.switch_to_tab(Tab.HELP)
+        self.initialize_event_stream()
 
         print("Web Viewer Client ready")
 
@@ -50,6 +61,52 @@ class WebViewerClient:
         self.current_tab = Tab.HELP
         jq(".tab").hide()
         jq(f"#{tab.value}").show()
+
+    def initialize_event_stream(self):
+        """
+        Start listening to the event stream from the running algorithm.
+        """
+        self.event_source = EventSource.new("/event_stream")
+        self.event_source.onmessage = self.on_message
+
+    def on_message(self, event):
+        """
+        Message received from the running algorithm.
+        The "event" parameter here isn't an algorithm event, but a message
+        from the web event stream.
+        """
+        message = jsjson.parse(event.data)
+        self.register_event(message.event)
+        self.register_stats(message.stats)
+
+    def register_event(self, event):
+        """
+        Register and show an algorithm event.
+        """
+        self.last_event = event
+        self.event_log.append(event)
+
+        event_html = self.event_as_html(event)
+        self.last_event_display.html(event_html)
+        self.log_display.append(event_html)
+
+    def register_stats(self, stats):
+        """
+        Register new stats from the algorithm.
+        """
+        self.stats = stats
+        self.stats_display.html("".join(
+            f"""<h2>{stat.name}</h2>
+                <p>{stat.value}</p>"""
+            for stat in stats
+        ))
+
+    def event_as_html(self, event):
+        """
+        Build the html for a single event.
+        """
+        return f"""<h2>{event.name}</h2>
+                   <p>{event.description}</p>"""
 
     def on_control_algorithm_click(self, e):
         """
