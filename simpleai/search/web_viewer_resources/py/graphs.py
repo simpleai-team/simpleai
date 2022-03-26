ce254be1987bbbca05465d2ec774b2c94e4b8dc3
GonzaloGV <gonzalovasquezher@gmail.com>
2022-03-26 19:42:12 -0300

    zoom added

--- simpleai/search/web_viewer_resources/py/graphs.py
+++ simpleai/search/web_viewer_resources/py/graphs.py
@@ -14,31 +14,41 @@ def render_graph(root_nodes, nodes_count, max_depth):
     """
     jq("#graph-image").html("")
 
+
     if not root_nodes:
         return
-
-    width = 1900
+    
+    width = 1900 
     height = 1000
 
     # initialize a d3 nodes hierarchy
-    treemap = d3.tree().size([height, width])
+    treemap = d3.tree().size([height, width])\
+        .separation(lambda a, b: 10)
     nodes = d3.hierarchy(root_nodes[0], lambda d: d.children)  # TODO what if more than one root?
     nodes = treemap(nodes)
+
+    # zoom
+    zoom = d3.zoom()\
+        .on('zoom', handle_zoom)
 
     # initialize the svg to draw on it with d3
     svg = d3.select("#graph-image").append("svg")\
             .attr("width", width)\
-            .attr("height", height + 30)\
+            .attr("height", height)\
+            .attr("viewbox", f"{width / 2 } {max_depth * 150} 0 0")
 
-    graph = svg.append("g")
+    # attach d3.zoom to svg figure
+    graph = svg.call(zoom)\
+        .append("g")\
+    
 
     # draw the links between nodes (before the nodes themselves, so the nodes come on top)
-    graph.selectAll(".link")\
+    links = graph.selectAll(".link")\
          .data(nodes.descendants()[1:])\
          .enter().append("path")\
          .attr("class", "link")\
          .attr("d", get_curved_path)
- 
+    
     # tie all the nodes to their data
     node = graph.selectAll(".node")\
                 .data(nodes.descendants())\
@@ -55,6 +65,10 @@ def render_graph(root_nodes, nodes_count, max_depth):
         .text(get_node_name)
 
     return
+
+
+def handle_zoom(e, *args):
+    d3.select("g").attr("transform", e.transform)
 
 
 def get_node_class(d, *args):
@@ -75,17 +89,19 @@ def translate_to_position(d, *args):
     """
     Translate a d3 element to the position of its related node.
     """
-    return f"translate({d.x},{d.y})"
+    y = d.depth * 150
+    x = d.x + (window.innerWidth / 2)
+    return f"translate({d.x},{y})"
 
 
 def get_curved_path(d, *args):
     """
     Get the parameters required for a curved path in a link.
     """
-    parent_y = d.parent.y 
-    parent_x = d.parent.x
-    y = d.y
-    x = d.x
+    parent_y = d.parent.depth * 150
+    parent_x = d.parent.x 
+    y = d.depth * 150
+    x = d.x 
 
     return (
         f"M{x},{y}" 
